a456dc5010ae41c980ac74937906d14788240bed
zhuguoqin <1906684772@qq.com>
2020-12-11 14:48:50 +0800

    添加影片信息

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const { response, request } = require('express');
 const express = require('express');
 // 引入数据
 const data = require('./data.json');
 const app = express();
+const bodyParser = require('body-parser')
+const fs = require('fs');
 
 app.set('view engine','ejs');
 app.set('views','./views');
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 // 创建中间件去获取影片详细信息
 function movieInfo(id){         
   for (var i=0; i<data.movies.length; i++ ){
@@ -16,12 +20,31 @@ function movieInfo(id){
   return;
 }
 // 视频路由页面 
-app.all('/practices/:id.html',(request,response) =>{
+app.get('/practices/:id.html',(request,response) =>{
   let id = request.params.id;
   let result = movieInfo(id); 
   response.render('app',{result,list:data.movies});
 })
+app.get('/practices/movie/creat',(request,response)=>{
+  response.render('movie/creat');
+})
+app.post('/movie/creat',(request,response)=>{
+  let arr = [];
+  data.movies.forEach(item=>{
+    arr.push(item.name)
+  })
+  // 获取请求体
+  if(request.body.name!== '' && arr.indexOf(request.body.name) == (-1)){
+    request.body.id = ++data.total;
+    data.movies.push(request.body);
+    fs.writeFileSync('./data.json',JSON.stringify(data));
+    response.send('添加成功')
+  }else(
+    response.send('请添加正确的影片信息') 
+  )
+})
+
 app.all('*',(request,response) =>{ 
   response.send('<h1>404 NotFound</h1>');
 })
-app.listen(8080);
+app.listen(80);
